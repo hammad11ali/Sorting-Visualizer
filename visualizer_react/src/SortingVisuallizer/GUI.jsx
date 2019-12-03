5c54a47173665ee292513942db4da3b90d4b6ce6
haseebvirk94 <haseebvirk94@gmail.com>
2019-12-03 15:04:01 +0500

    Fixed GUI
    Added integration for array size slider and drop down for algorithms.

--- visualizer_react/src/SortingVisuallizer/GUI.jsx
+++ visualizer_react/src/SortingVisuallizer/GUI.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import Navbar from './Navbar';
-import Footer from './Footer';
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 import "./SortingVisuallizer.css";
 import bubbleSort from "../SortingAlgorithm/bubbleSort.js";
 import InsertionSort from "../SortingAlgorithm/InsertionSort.js";
@@ -21,163 +21,195 @@ const SECONDARY_COLOR = "red";
 const TERTIARY_COLOR = "green";
 
 class GUI extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            Randomarray: []
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      arraySize: 10,
+      Randomarray: []
+    };
+  }
+  componentDidMount() {
+    this.resetArray(this.state.arraySize);
+    this.handleArraySize(this.state.arraySize);
+  }
+  resetArray = size => {
+    const array = [];
+    for (let i = 0; i < size; i++) {
+      array.push(randomIntFromInterval(5, 25));
     }
-    componentDidMount() {
-        this.resetArray();
+    this.setState({
+      Randomarray: array
+    });
+  };
+  handleResetArray = () => {
+    this.resetArray(this.state.arraySize);
+  };
+  handleArraySize = aSize => {
+    this.setState({
+      arraySize: aSize
+    });
+    this.resetArray(aSize);
+  };
+  handleSort = index => {
+    switch (index) {
+      case 0:
+        //merge Sort
+        break;
+      case 1:
+        //quick Sort
+        break;
+      case 2:
+        //selection Sort
+        this.selectionSort_animate();
+        break;
+      case 3:
+        //shell Sort
+        this.shellSort_animate();
+        break;
+      case 4:
+        //odd-even Sort
+        this.oddEvenSort_animate();
+        break;
+      case 5:
+        //bubble Sort
+        this.bubbleSort_animate();
+      case 6:
+        //Cocktail Sort
+        this.CocktailSort_animate();
+        break;
+      case 7:
+        //comb Sort
+        this.CombSort_animate();
+        break;
+      case 8:
+        //gnome Sort
+        this.GnomeSort_animate();
+        break;
+      case 9:
+        //insertion Sort
+        this.InsertionSort_animate();
+        break;
+
+      default:
+        break;
     }
-    resetArray() {
-        const array = [];
-        for (let i = 0; i < 10; i++) {
-            array.push(randomIntFromInterval(5, 25));
-        }
-        this.setState({
-            Randomarray: array
-        });
+    // case switch to call required sort
+  };
+  quickSort() {
+    // We leave it as an exercise to the viewer of this code to implement this method.
+  }
+
+  heapSort() {
+    // We leave it as an exercise to the viewer of this code to implement this method.
+  }
+  general_Animate(animation) {
+    var j = 1;
+    let array_bar = document.getElementsByClassName("array-bar");
+    for (var i = 0; i < animation.length; i++) {
+      let current = animation[i];
+      setTimeout(() => {
+        array_bar[current[0]].style.backgroundColor = SECONDARY_COLOR;
+        array_bar[current[1]].style.backgroundColor = SECONDARY_COLOR;
+      }, ANIMATION_SPEED_MS * j);
+      j++;
+      if (current[2]) {
+        setTimeout(() => {
+          array_bar[current[0]].style.backgroundColor = TERTIARY_COLOR;
+          array_bar[current[1]].style.backgroundColor = TERTIARY_COLOR;
+        }, ANIMATION_SPEED_MS * j);
+        j++;
+        setTimeout(() => {
+          let temp = array_bar[current[0]].innerHTML;
+          array_bar[current[0]].innerHTML = array_bar[current[1]].innerHTML;
+          array_bar[current[1]].innerHTML = temp;
+          array_bar[current[0]].style.height = `${array_bar[current[0]]
+            .childNodes[0].innerHTML * 20}px`;
+          array_bar[current[1]].style.height = `${array_bar[current[1]]
+            .childNodes[0].innerHTML * 20}px`;
+          array_bar[current[0]].style.backgroundColor = PRIMARY_COLOR;
+          array_bar[current[1]].style.backgroundColor = PRIMARY_COLOR;
+        }, ANIMATION_SPEED_MS * j);
+        j++;
+      } else
+        setTimeout(() => {
+          array_bar[current[0]].style.backgroundColor = PRIMARY_COLOR;
+          array_bar[current[1]].style.backgroundColor = PRIMARY_COLOR;
+        }, ANIMATION_SPEED_MS * j);
+      j++;
     }
-
-    quickSort() {
-        // We leave it as an exercise to the viewer of this code to implement this method.
-    }
-
-    heapSort() {
-        // We leave it as an exercise to the viewer of this code to implement this method.
-    }
-    general_Animate(animation) {
-        var j = 1;
-        let array_bar = document.getElementsByClassName("array-bar");
-        for (var i = 0; i < animation.length; i++) {
-            let current = animation[i];
-            setTimeout(() => {
-                array_bar[current[0]].style.backgroundColor = SECONDARY_COLOR;
-                array_bar[current[1]].style.backgroundColor = SECONDARY_COLOR;
-            }, ANIMATION_SPEED_MS * j);
-            j++;
-            if (current[2]) {
-                setTimeout(() => {
-                    array_bar[current[0]].style.backgroundColor = TERTIARY_COLOR;
-                    array_bar[current[1]].style.backgroundColor = TERTIARY_COLOR;
-                }, ANIMATION_SPEED_MS * j);
-                j++;
-                setTimeout(() => {
-                    let temp = array_bar[current[0]].innerHTML;
-                    array_bar[current[0]].innerHTML = array_bar[current[1]].innerHTML;
-                    array_bar[current[1]].innerHTML = temp;
-                    array_bar[current[0]].style.height = `${array_bar[current[0]]
-                        .childNodes[0].innerHTML * 20}px`;
-                    array_bar[current[1]].style.height = `${array_bar[current[1]]
-                        .childNodes[0].innerHTML * 20}px`;
-                    array_bar[current[0]].style.backgroundColor = PRIMARY_COLOR;
-                    array_bar[current[1]].style.backgroundColor = PRIMARY_COLOR;
-                }, ANIMATION_SPEED_MS * j);
-                j++;
-            } else
-                setTimeout(() => {
-                    array_bar[current[0]].style.backgroundColor = PRIMARY_COLOR;
-                    array_bar[current[1]].style.backgroundColor = PRIMARY_COLOR;
-                }, ANIMATION_SPEED_MS * j);
-            j++;
-        }
-    }
-    bubbleSort_animate = () => {
-        let animation = bubbleSort(this.state.Randomarray);
-        this.general_Animate(animation);
-    };
-    InsertionSort_animate = () => {
-        let animation = InsertionSort(this.state.Randomarray);
-        this.general_Animate(animation);
-    };
-    shellSort_animate = () => {
-        console.log("shell");
-        let animation = ShellSort(this.state.Randomarray);
-        console.log(animation);
-        this.general_Animate(animation);
-    };
-    oddEvenSort_animate = () => {
-        let animation = oddEvenSort(this.state.Randomarray);
-        this.general_Animate(animation);
-    };
-    selectionSort = () => {
-        let animation = SelectionSort(this.state.Randomarray);
-        this.general_Animate(animation);
-    };
-    CocktailSort_animate = () => {
-        let animation = CocktailSort(this.state.Randomarray);
-        this.general_Animate(animation);
-    };
-    CombSort_animate = () => {
-        let animation = CombSort(this.state.Randomarray);
-        this.general_Animate(animation);
-    };
-    GnomeSort_animate = () => {
-        let animation = GnomeSort(this.state.Randomarray);
-        this.general_Animate(animation);
-    };
-    render() {
-        return (
-            <div className="GUI" >
-
-                <div>
-                    <Navbar />
-                </div>
-                <div className="array-container">
-                    {this.state.Randomarray.map((value, idx) => (
-                        <div
-                            className="array-bar"
-                            key={idx}
-                            style={{ height: `${value * 20}px` }}
-                        >
-                            <p>{value}</p>
-                        </div>
-                    ))}
-                    <button onClick={() => this.resetArray()}>Generate New Array</button>
-                    <button onClick={() => this.mergeSort()}>Merge Sort</button>
-                    <button onClick={() => this.quickSort()}>Quick Sort</button>
-                    <button onClick={() => this.heapSort()}>Heap Sort</button>
-                    <button onClick={() => this.selectionSort()}>Selection Sort</button>
-                    <button onClick={() => this.shellSort_animate()}>Shell Sort</button>
-                    <button onClick={() => this.oddEvenSort_animate()}>Odd-Even Sort</button>
-                    <button onClick={() => this.bubbleSort_animate()}>Bubble Sort</button>
-                    <button onClick={() => this.CocktailSort_animate()}>CocktailSort</button>
-                    <button onClick={() => this.CombSort_animate()}>Comb Sort</button>
-                    <button onClick={() => this.GnomeSort_animate()}>Gnome Sort</button>
-                    <button onClick={() => this.InsertionSort_animate()}>Insertion Sort</button>
-                    <button onClick={() => this.testSortingAlgorithms()}>Test Sorting Algorithms (BROKEN)</button>
-                </div>
-
-                <div>
-                    <Footer />
-                </div>
-
-
-
+  }
+  bubbleSort_animate = () => {
+    let animation = bubbleSort(this.state.Randomarray);
+    this.general_Animate(animation);
+  };
+  InsertionSort_animate = () => {
+    let animation = InsertionSort(this.state.Randomarray);
+    this.general_Animate(animation);
+  };
+  shellSort_animate = () => {
+    console.log("shell");
+    let animation = ShellSort(this.state.Randomarray);
+    console.log(animation);
+    this.general_Animate(animation);
+  };
+  oddEvenSort_animate = () => {
+    let animation = oddEvenSort(this.state.Randomarray);
+    this.general_Animate(animation);
+  };
+  selectionSort_animate = () => {
+    let animation = SelectionSort(this.state.Randomarray);
+    this.general_Animate(animation);
+  };
+  CocktailSort_animate = () => {
+    let animation = CocktailSort(this.state.Randomarray);
+    this.general_Animate(animation);
+  };
+  CombSort_animate = () => {
+    let animation = CombSort(this.state.Randomarray);
+    this.general_Animate(animation);
+  };
+  GnomeSort_animate = () => {
+    let animation = GnomeSort(this.state.Randomarray);
+    this.general_Animate(animation);
+  };
+  render() {
+    return (
+      <div className="GUI">
+        <div>
+          <Navbar
+            handleAlgorithm={this.handleSort}
+            handleRandom={this.handleResetArray}
+          />
+        </div>
+        <div className="array-container">
+          {this.state.Randomarray.map((value, idx) => (
+            <div
+              className="array-bar"
+              key={idx}
+              style={{ height: `${value * 20}px` }}
+            >
+              <p>{value}</p>
             </div>
-
-
-        )
-
-
-    }
-
+          ))}
+        </div>
+        <div>
+          <Footer handleSize={this.handleArraySize} />
+        </div>
+      </div>
+    );
+  }
 }
 export default GUI;
 
-
-
-
 function randomIntFromInterval(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 function arraysAreEqual(arrayOne, arrayTwo) {
-    if (arrayOne.length !== arrayTwo.length) return false;
-    for (let i = 0; i < arrayOne.length; i++) {
-        if (arrayOne[i] !== arrayTwo[i]) {
-            return false;
-        }
+  if (arrayOne.length !== arrayTwo.length) return false;
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
     }
-    return true;
+  }
+  return true;
 }
--- visualizer_react/src/SortingVisuallizer/Navbar.js
+++ visualizer_react/src/SortingVisuallizer/Navbar.js
@@ -1,45 +1,64 @@
-import React from 'react';
-import './Navbar.css'
+import React from "react";
+import "./Navbar.css";
 
 const navbar = props => (
-    <header>
-        <nav className="navbar navbar-expand-lg navbar-dark">
-            <a className="navbar-brand">Sorting Visualizer</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
+  <header>
+    <nav className="navbar navbar-expand-lg navbar-dark">
+      <a className="navbar-brand">Sorting Visualizer</a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+          <li>
+            <button type="button" onClick={props.handleRandom} className="btn">
+              Random Array
             </button>
-
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
-                    <li>
-                        <button type="button" className="btn">Random Array</button>
-                    </li>
-                    <li>
-                        <select id="inputState" className="form-control" placeholder="choose">
-                            <option value="">Merge Sort</option>
-                            <option>Quick Sort</option>
-                            <option>Quick Sort</option>
-                            <option>Heap Sort</option>
-                            <option>Selection Sort</option>
-                            <option>Shell Sort</option>
-                            <option>Odd-Even Sort</option>
-                            <option>Bubble Sort</option>
-                            <option>Cocktail Sort</option>
-                            <option>Comb Sort</option>
-                            <option>Gnome Sort</option>
-                            <option>Insertion Sort</option>
-                        </select>
-                    </li>
-                    <li>
-                        <button type="button" className="btn">Sort</button>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-
-    </header>
-
+          </li>
+          <li>
+            <select
+              id="inputState"
+              className="form-control"
+              placeholder="choose"
+            >
+              <option value="0">Merge Sort</option>
+              <option value="1">Quick Sort</option>
+              <option value="2">Selection Sort</option>
+              <option value="3">Shell Sort</option>
+              <option value="4">Odd-Even Sort</option>
+              <option value="5">Bubble Sort</option>
+              <option value="6">Cocktail Sort</option>
+              <option value="7">Comb Sort</option>
+              <option value="8">Gnome Sort</option>
+              <option value="9">Insertion Sort</option>
+            </select>
+          </li>
+          <li>
+            <button
+              type="button"
+              onClick={() =>
+                props.handleAlgorithm(
+                  document.getElementById("inputState").selectedIndex
+                )
+              }
+              className="btn"
+            >
+              Sort
+            </button>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
 );
 
-export default navbar
+export default navbar;
